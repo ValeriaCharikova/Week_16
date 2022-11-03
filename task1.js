e19e252ceb8e3bfc349463b607126f356da96ae6
Vlb92 <valeriiacharikova@gmail.com.com>
2022-11-03 22:58:20 +0300

    added task 1

---
+++ task1.js
@@ -0,0 +1,8 @@
+function sumInput() {
+  let numbers = [];
+  let a = document.getElementById("first_elem").value;
+  let b = document.getElementById("second_elem").value;
+  let c = document.getElementById("third_elem").value;
+  document.getElementById("sort").innerHTML = ;
+  document.getElementById("sum").innerHTML = Number(a) + Number(b) + Number(c);
+}
