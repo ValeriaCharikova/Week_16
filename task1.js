bc01f6199a3c9ad23d6abecee157406908ab2b0a
Vlb92 <valeriiacharikova@gmail.com.com>
2022-11-04 15:51:37 +0300

    fixed the function

--- task1.js
+++ task1.js
@@ -3,6 +3,9 @@ function sumInput() {
   let a = document.getElementById("first_elem").value;
   let b = document.getElementById("second_elem").value;
   let c = document.getElementById("third_elem").value;
-  document.getElementById("sort").innerHTML = ;
+  numbers.push(Number(a));
+  numbers.push(Number(b));
+  numbers.push(Number(c));
+  document.getElementById("sort").innerHTML = numbers.sort();
   document.getElementById("sum").innerHTML = Number(a) + Number(b) + Number(c);
 }
